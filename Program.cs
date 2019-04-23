fa77f58f76812279e78bf053572b591bce030f09
Horvath <Christine.Horvath@vertivco.com>
2019-04-23 15:11:44 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're 'ACCOUNTING' on you!");
+            Console.ReadLine();
+        }
+    }
+}
