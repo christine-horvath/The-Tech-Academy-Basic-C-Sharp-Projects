d429615cee802b78d91271e8f5e1c1bd3fcb0526
Horvath <Christine.Horvath@vertivco.com>
2019-04-24 11:45:49 -0400

    anon income comparison

---
+++ AnonIncomeComparison/AnonIncomeComparison.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonIncomeComparison
+{
+    class AnonIncomeComparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("What is Person 1's hourly rate?");
+            decimal paHourly = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("How many hours does Person 1 work per week?");
+            decimal paHours = Decimal.Parse(Console.ReadLine());
+            decimal paAnnual = (paHourly * paHours * 52);
+            //Console.WriteLine("Person 2:");
+            //Console.WriteLine("What is Person 2's hourly rate?");
+            //int pbHourly = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("How many hours does Person 2 work per week?");
+            //int pbHours = Convert.ToInt32(Console.ReadLine());
+            //int pbAnnual = (pbHourly * pbHours * 52);
+            //Console.WriteLine("Annual Salary of Person 1");
+            //Console.WriteLine(paAnnual);
+            //Console.WriteLine("Annual Salary of Person 2");
+            //Console.WriteLine(pbAnnual);
+            //Console.WriteLine("Does Person 1 make more money than Person 2?");
+            //bool annualSalary = paAnnual > pbAnnual;
+            //Console.WriteLine(annualSalary);
+            //Console.ReadLine();
+        }
+    }
+}
