1b6abc6705bbd9c568c96f0315fe7f0eec0e9262
Horvath <Christine.Horvath@vertivco.com>
2019-04-24 10:49:05 -0400

    operators and comparisons addition

---
+++ ComparisonsAndOperators/OperatorsExercise.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExercise
+{
+    class OperatorsExercise
+    {
+        static void Main(string[] args)
+        {
+            //1.Takes an input from the user, multiplies it by 50, and prints the result to the console. (Note: make sure your code can take inputs larger than 10, 000, 000).
+
+            //Console.WriteLine("Please enter a number: ");
+            //double userInput = double.Parse(Console.ReadLine());
+            //double multBy = 50;
+            //double userOutput = userInput * multBy;
+            //Console.WriteLine("Your number times fifty = " + userOutput);
+            //Console.ReadLine();
+
+            //2.Takes an input from the user, adds 25 to it, and prints the result to the console.
+
+            //Console.WriteLine("Please enter a number: ");
+            //int addInput = int.Parse(Console.ReadLine());
+            //int addNum = 25;
+            //double addOutput = addInput + addNum;
+            //Console.WriteLine("Your number plus 25 = " + addOutput);
+            //Console.ReadLine();
+
+            //3.Takes an input from the user, divides it by 12.5, and prints the result to the console.
+
+            //Console.WriteLine("Please enter a number: ");
+            //int divInput = int.Parse(Console.ReadLine());
+            //decimal divNum = 12.5m;
+            //decimal divOutput = divInput / divNum;
+            //Console.WriteLine("Your number divided by 12.5 = " + divOutput);
+            //Console.ReadLine();
+
+            //4.Takes an input from the user, checks if it is greater than 50, and prints the true / false result to the console.
+
+            //Console.WriteLine("Please enter a number: ");
+            //int boolInput = int.Parse(Console.ReadLine());
+            //bool isGreater = boolInput > 50;
+
+            //Console.WriteLine("Your number is greater than 50: " + isGreater);
+            //Console.ReadLine();
+
+            //5.Takes an input from the user, divides it by 7, and prints the remainder to the console(tip: think % operator).
+
+            //Console.WriteLine("Please enter a number: ");
+            //int modInput = int.Parse(Console.ReadLine());
+            //int remainder  = modInput % 2;
+
+            //Console.WriteLine("The remainder of your number divided by 2 = " + remainder);
+            //Console.ReadLine();
+        }
+    }
+}
