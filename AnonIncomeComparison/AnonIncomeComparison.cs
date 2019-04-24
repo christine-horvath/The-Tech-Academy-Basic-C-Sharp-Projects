be698d5f3d6f2cfa836884b04655c9607f468426
Horvath <Christine.Horvath@vertivco.com>
2019-04-24 11:57:59 -0400

    update with decimals

--- AnonIncomeComparison/AnonIncomeComparison.cs
+++ AnonIncomeComparison/AnonIncomeComparison.cs
@@ -13,24 +13,24 @@ namespace AnonIncomeComparison
             Console.WriteLine("Anonymous Income Comparison Program");
             Console.WriteLine("Person 1:");
             Console.WriteLine("What is Person 1's hourly rate?");
-            decimal paHourly = Decimal.Parse(Console.ReadLine());
+            decimal paHourly = decimal.Parse(Console.ReadLine());
             Console.WriteLine("How many hours does Person 1 work per week?");
-            decimal paHours = Decimal.Parse(Console.ReadLine());
+            int paHours = Convert.ToInt32(Console.ReadLine());
             decimal paAnnual = (paHourly * paHours * 52);
-            //Console.WriteLine("Person 2:");
-            //Console.WriteLine("What is Person 2's hourly rate?");
-            //int pbHourly = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("How many hours does Person 2 work per week?");
-            //int pbHours = Convert.ToInt32(Console.ReadLine());
-            //int pbAnnual = (pbHourly * pbHours * 52);
-            //Console.WriteLine("Annual Salary of Person 1");
-            //Console.WriteLine(paAnnual);
-            //Console.WriteLine("Annual Salary of Person 2");
-            //Console.WriteLine(pbAnnual);
-            //Console.WriteLine("Does Person 1 make more money than Person 2?");
-            //bool annualSalary = paAnnual > pbAnnual;
-            //Console.WriteLine(annualSalary);
-            //Console.ReadLine();
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("What is Person 2's hourly rate?");
+            decimal pbHourly = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("How many hours does Person 2 work per week?");
+            int pbHours = Convert.ToInt32(Console.ReadLine());
+            decimal pbAnnual = (pbHourly * pbHours * 52);
+            Console.WriteLine("Annual Salary of Person 1");
+            Console.WriteLine(paAnnual);
+            Console.WriteLine("Annual Salary of Person 2");
+            Console.WriteLine(pbAnnual);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool annualSalary = paAnnual > pbAnnual;
+            Console.WriteLine(annualSalary);
+            Console.ReadLine();
         }
     }
 }
