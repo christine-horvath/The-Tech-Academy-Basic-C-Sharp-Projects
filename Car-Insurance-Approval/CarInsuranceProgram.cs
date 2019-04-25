e0b8dd9b277f097f7877ff152f124546e8793edb
Horvath <Christine.Horvath@vertivco.com>
2019-04-24 21:02:13 -0400

    car insurance project

---
+++ Car-Insurance-Approval/CarInsuranceProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class CarInsuranceProgram
+    {
+        static void Main(string[] args)
+        {
+
+            //What is your age ?
+
+            //Have you ever had a DUI?
+
+            //How many speeding tickets do you have?
+
+            //Use the following qualification rules to determine if the applicant qualifies for car insurance:
+
+            //Applicants must be over 15 years old.
+
+            //Applicants must not have any DUI’s.
+
+            //Applicants must not have more than 3 speeding tickets.
+
+            //Print the result of the boolean expression created from the above business rules.
+
+
+            Console.WriteLine("What is your age?");
+            int enterAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? 'true or false'.");
+            string hasDui = Console.ReadLine();
+            bool dui = true;
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int hasTickets = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine("Does applicant qualify for car insurance?");
+            bool result = (enterAge > 15 && dui && hasTickets <3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
