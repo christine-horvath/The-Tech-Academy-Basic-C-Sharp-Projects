c2796e47534f8a2e358b6e704fe29390c0a51c0d
Christine <you@example.com>
2019-05-02 14:27:13 -0400

    class app commit 1

---
+++ Class App/Lattes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_App
+{
+    public class Lattes
+    {
+        public int AlmondMilk(int cream, int sugar)
+        {
+            int AlmondMilk = (cream) - (sugar);
+            return AlmondMilk;
+        }
+
+        public double AlmondMilk(double cream, double syrup, double sugar)
+        {
+            double AlmondMilk = cream * syrup * sugar;
+            return AlmondMilk;
+
+        }
+
+        public string AlmondMilk(string cream, string sugar)
+        {
+            string AlmondMilk = (cream) + (sugar);
+
+            {
+                if (Int32.TryParse(AlmondMilk, out int numValue))
+                {
+                    return AlmondMilk;
+                }
+                else
+
+                    Console.WriteLine($"Int32.TryParse could not parse the following to an integer: " + AlmondMilk);
+
+            }
+            return AlmondMilk;
+        }
+    }
+}
---
+++ Class App/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter an Odd Number");
+            //int cream = Convert.ToInt32(Console.ReadLine());
+            //int sugar = 2;
+
+            //Lattes latte = new Lattes();
+            //latte.AlmondMilk(cream, sugar);
+            //Console.WriteLine("Your total of creams plus sugars is " + latte.AlmondMilk(cream, sugar));
+            //Console.ReadLine();
+
+            ////////////////////////////////////////////////////
+
+            //Console.WriteLine("Enter a decimal");
+            //double cream = Convert.ToDouble(Console.ReadLine());
+            //double sugar = 2;
+            //double syrup = 5;
+
+            ////Lattes latte = new Lattes();
+            ////latte.AlmondMilk(cream, sugar);
+            ////Console.WriteLine("Your total of creams plus sugars is " + latte.AlmondMilk(cream, sugar));
+            ////Console.ReadLine();
+
+            //Lattes latte = new Lattes();
+            //latte.AlmondMilk(cream, syrup, sugar);
+            //Console.WriteLine(Convert.ToInt64(latte.AlmondMilk(cream, syrup, sugar)));
+            //Console.ReadLine();
+
+            //////////////////////////////////////////////////////
+            
+            Console.WriteLine("Enter a number");
+            string cream = (Console.ReadLine());
+            string sugar = "sugars";
+
+            Lattes latte = new Lattes();
+            latte.AlmondMilk(cream, sugar);
+            Console.WriteLine("Your number plus the string = " + (Convert.ToInt64(latte.AlmondMilk(cream, sugar))));
+            Console.ReadLine();
+
+        }
+    }
+}
