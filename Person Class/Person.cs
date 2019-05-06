a6f84cd2895ee9b4c7741b650b01fa8a5bfee55f
Christine <you@example.com>
2019-05-06 12:36:11 -0400

    Person abstract class commit

--- Person Class/Person.cs
+++ Person Class/Person.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace Person_Class
 {
-    public class Person
+    public abstract class Person
     {
 
         //first name, last name
         public List<string> FirstName { get; set; }
         public List<string> LastName { get; set; }
 
-        //
-        public void SayName()
+        //1. Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+        //2. Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: "Name: [full name]".
+        public virtual void SayName()
         {
             foreach (string firstname in FirstName)
             {
