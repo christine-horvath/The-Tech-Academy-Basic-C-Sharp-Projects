c24ecdd208739002281f5c7223c43c1d96b58a53
Christine <you@example.com>
2019-05-09 12:42:50 -0400

    datetime first commit

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -14,15 +14,22 @@ namespace FileIO
             //1.Ask a user for a number.
             //2.Log that number to a text file.
             //3.Print the text file back to the user.
+            //Console.WriteLine("Please enter a number.");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //string text = userInput.ToString();
+            //File.WriteAllText(@"C:\Users\Christine.Horvath\logs\log.txt", text);
+            //Console.Write("The number you entered is " + (File.ReadAllText(@"C:\Users\Christine.Horvath\logs\log.txt") + "."));
+            //Console.Write(File.ReadAllText(@"C:\Users\Christine.Horvath\logs\log.txt"));
+            //Console.ReadLine();
+
+            DateTime currentTime = DateTime.Now;            
+            Console.WriteLine(DateTime.Now);
+            //Console.ReadLine();
             Console.WriteLine("Please enter a number.");
             int userInput = Convert.ToInt32(Console.ReadLine());
-            string text = userInput.ToString();
-            File.WriteAllText(@"C:\Users\Christine.Horvath\logs\log.txt", text);
-            Console.Write("The number you entered is " + (File.ReadAllText(@"C:\Users\Christine.Horvath\logs\log.txt") + "."));
-            //Console.Write(File.ReadAllText(@"C:\Users\Christine.Horvath\logs\log.txt"));
+            DateTime plusUserInputHours = currentTime.AddHours(userInput);
+            Console.WriteLine("In " + userInput + " hours, the time will be " + string.Format("{0}", plusUserInputHours + "."));
             Console.ReadLine();
-
-            //use @ or \\ to write file location
 
 
         }
