c9b4f5c2a0728fdc2cb2fa0b0f0e5fa25249868f
Christine <you@example.com>
2019-05-14 14:56:07 -0400

    quote gen 3

---
+++ QuoteGen/quoteGeneratorX/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿
+using quoteGeneratorX.Models;
+using quoteGeneratorX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuoteGen.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+            using (quoteGenEntities db = new quoteGenEntities())
+            {
+                var applicants = (from c in db.driverDatas
+                                  where c.Id < 1000
+                                  select c).ToList();
+                var applicantVms = new List<ApplicantVM>();
+                foreach (var applicant in applicants)
+                {
+                    var applicantVm = new ApplicantVM();
+                    applicantVm.Id = applicant.Id;
+                    applicantVm.Firstname = applicant.FirstName;
+                    applicantVm.LastName = applicant.LastName;
+                    applicantVm.EmailAddress = applicant.EmailAddress;
+                    applicantVms.Add(applicantVm);
+                }
+                return View(applicantVms);
+            }
+        }
+        public ActionResult Quote(int Id)
+        {
+            using (quoteGenEntities db = new quoteGenEntities())
+            {
+                var applicant = db.driverDatas.Find(Id);
+                applicant.Id = Id;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QuoteGen/quoteGeneratorX/Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿
+using quoteGeneratorX.Models;
+using quoteGeneratorX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quoteGeneratorX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Quote(string firstname, string lastName, string emailAddress, DateTime dateOfBirth, int carYear, string carMake, string carModel, bool hasDUI, int numberofTickets, bool fullCoverage)
+        {
+
+            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel))
+            {
+                return View("~Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                double quote = 50;
+                var today = DateTime.Today;
+                int age = today.Year - dateOfBirth.Year;
+
+
+                //----------------------------------
+                if (age < 25)
+                {
+                    quote += 25;
+                }
+                else if (age < 18)
+                {
+                    quote += 100;
+                }
+                else if (age > 100)
+                {
+                    quote += 25;
+
+                }
+                //----------------------------------------------
+                if (carYear < 2000)
+                {
+                    quote += 25;
+                }
+
+                //---------------------------------------------
+                if (carMake.ToLower() == "porsche")
+                {
+                    //add "to lower"
+                    quote += 25;
+                }
+
+                //--------------------------------------------
+                if (carMake.ToLower() == "porsche" && carModel.ToLower() == "911 carrera")
+                {
+
+                    quote += 25;
+                }
+
+                //----------------------------------------------
+                if (numberofTickets > 0)
+                {
+                    quote += (numberofTickets * 10);
+                }
+                //----------------------------------------------
+                if (hasDUI == true)
+                {
+                    quote *= 1.25;
+
+                }
+                //----------------------------------------------
+                if (fullCoverage == true)
+                {
+                    quote *= 1.5;
+                }
+
+                using (quoteGenEntities db = new quoteGenEntities())
+                {
+                    
+                    var driverdata = new driverData();
+                    driverdata.FirstName = firstname;
+                    driverdata.LastName = lastName;
+                    driverdata.EmailAddress = emailAddress;
+                    driverdata.BirthDate = dateOfBirth;
+                    driverdata.CarYear = carYear;
+                    driverdata.CarMake = carMake;
+                    driverdata.CarModel = carModel;
+                    driverdata.HasDUI = hasDUI;
+                    driverdata.NumberOfTickets = numberofTickets;
+                    driverdata.FullCoverage = fullCoverage;
+
+                    db.driverDatas.Add(driverdata);
+                    db.SaveChanges();
+
+                    var quoteeVms = new List<QuoteReturnVM>();
+                    foreach (var quoteeVM in quoteeVms)
+                    {
+                        quoteeVM.Firstname = driverdata.FirstName;
+                        quoteeVM.LastName = driverdata.LastName;
+                        quoteeVM.EmailAddress = driverdata.EmailAddress;
+                        quoteeVms.Add(quoteeVM);
+                            
+                    }
+
+                    return View(quoteeVms);                    
+                } 
+            }        
+        }
+        public ActionResult Quote(int Id)
+        {
+            using (quoteGenEntities db = new quoteGenEntities())
+            {
+                var applicant = db.driverDatas.Find(Id);
+            }
+            return RedirectToAction("Success");
+        }
+    }
+}
+
+    
