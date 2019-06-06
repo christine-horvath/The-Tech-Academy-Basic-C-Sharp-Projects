7a0372db85a2eb007b1bff2e82f9844d05e4cf63
Christine <you@example.com>
2019-06-06 13:51:16 -0400

    technical questions

---
+++ technical questions/TechQuestions.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//namespace technical_questions
+//{
+public class Program
+{
+    public static void Main()
+    {
+        //Given an array of integers, write a method to total the odd numbers.
+        //int i,n,sum=0;
+        //Console.Write("\n\n");
+        //Console.Write("Display the sum of n odd natural number:\n");
+        //Console.Write("---------------------------");
+        //Console.Write("\n\n");
+
+        //Console.Write("Input number of terms : ");
+        //n = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("\nThe odd numbers are : ");
+        //for(i = 1; i <= n ; i++)
+        //{
+        //    Console.Write("{0} ", 2 * i - 1);
+        //    sum += 2 * i - 1;
+        //}
+        //Console.Write("\nThe sum of odd natural number upto {0} terms : {1} \n", n, sum);
+        //Console.ReadLine();
+
+        //Given an array of integers, write a method to sum the elements in the array, knowing that some of the elements may be very large integers.
+        //** Couldn't get DOUBLE to work, so i just went with int ** //
+        //int[] a = new int[100];
+        //int i, n, sum = 0;
+
+
+        //Console.Write("\n\nFind sum of all elements of array:\n");
+        //Console.Write("--------------------------------------\n");
+
+        //Console.Write("Input the number of elements to be stored in the array :");
+        //n = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("Input {0} elements in the array :\n", n);
+        //for (i = 0; i < n; i++)
+        //{
+        //    Console.Write("element - {0} : ", i);
+        //    a[i] = Convert.ToInt32(Console.ReadLine());
+        //}
+
+        //for (i = 0; i < n; i++)
+        //{
+        //    sum += a[i];
+        //}
+
+        //Console.Write("Sum of all elements stored in the array is : {0}\n\n", sum);
+        //Console.ReadLine();
+
+        //Given a string, reverse it.
+        //public static string ReverseString(string s)
+        //{
+        //    char[] arr = s.ToCharArray();
+        //    Array.Reverse(arr);
+        //    return new string(arr);
+        //}
+        //static void Main()
+        //{
+        //    string userString;
+        //    Console.WriteLine("Enter string");
+        //    userString = Console.ReadLine();
+        //    Console.Write("Your string reversed is: ");
+        //    Console.Write(Program.ReverseString(userString));
+        //    Console.ReadLine();
+
+        //Given a string, remove any repeated letters.
+        //public static string RemoveDuplicates(string s)
+        //{
+        //    return new string(s.ToCharArray().Distinct().ToArray());
+        //}
+        //static void Main()
+        //{
+        //    string userString;
+        //    Console.WriteLine("Enter string");
+        //    userString = Console.ReadLine();
+        //    Console.Write("Your string without duplicates is: ");
+        //    Console.Write(Program.RemoveDuplicates(userString));
+        //    Console.ReadLine();
+        //}
+
+        //FizzBuzz (Look this one up and try it out)
+        //int n = 100;
+
+        //// loop for 100 times 
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    // number divisible by 5,  
+        //    // print 'Buzz' in place 
+        //    // of the number 
+        //    if (i % 15 == 0)
+        //        Console.Write("FizzBuzz" + " ");
+
+        //    // number divisible by 3,  
+        //    // print 'Fizz' in place  
+        //    // of the number 
+        //    else if (i % 3 == 0)
+        //        Console.Write("Fizz" + " ");
+
+        //    // number divisible by  
+        //    // 15(divisible by both  
+        //    // 3 & 5), print 'FizzBuzz' 
+        //    // in place of the number 
+        //    else if (i % 5 == 0)
+        //        Console.Write("Buzz" + " ");
+
+        //    // print the numbers  
+        //    else
+        //        Console.Write(i + " ");
+
+        //    Console.ReadLine();
+        }
+    }
+}
+
+
+
+
